86f2c5dd783b17bb531f4df92bce727cf2555821
nathalyamessias <nathalya.messias@iesb.edu.br>
2026-05-12 09:07:45 -0300

    feat: conclui pratica06. Fecha #12

--- praticas/aluno-online-react/src/App.jsx
+++ praticas/aluno-online-react/src/App.jsx
@@ -1,7 +1,21 @@
-import "./App.css";
+import { Routes, Route } from 'react-router';
+import Layout from './Components/Layout/Layout';
+import Dashboard from './Pages/Dashboard';
+import Faltas from './Pages/Faltas';
+import Notas from './Pages/Notas';
+import Boletos from './Pages/Boletos';
+import Requerimentos from './Pages/Requerimentos';
 
-function App() {
-  return <></>;
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Dashboard />} />
+        <Route path="faltas" element={<Faltas />} />
+        <Route path="notas" element={<Notas />} />
+        <Route path="boletos" element={<Boletos />} />
+        <Route path="requerimentos" element={<Requerimentos />} />
+      </Route>
+    </Routes>
+  );
 }
-
-export default App;
---
+++ praticas/aluno-online-react/src/components/TextoDescritivo.jsx
@@ -0,0 +1,7 @@
+export default function TextoDescritivo({ texto }) {
+  return (
+    <h2 className="text-[1.8rem] font-bold text-[#303030] mb-8 -mt-6">
+      {texto}
+    </h2>
+  );
+}
